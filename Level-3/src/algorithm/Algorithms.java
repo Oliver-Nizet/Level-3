9ef13031aa067f91c851aa73dcd23ab9151edb29
Oliver Nizet <League@AdministorsiMac.attlocal.net>
2018-03-06 17:38:30 -0800

    asdf

---
+++ Level-3/src/algorithm/Algorithms.java
@@ -0,0 +1,12 @@
+package algorithm;
+
+import java.util.List;
+
+public class Algorithms {
+
+	public static int findBrokenEgg(List<String> eggs) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
