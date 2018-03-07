05faec34b69e8e036ce6d7bf7cf6033e215f88b4
Oliver Nizet <League@AdministorsiMac.attlocal.net>
2018-03-06 18:36:30 -0800

    hi

--- Level-3/src/algorithm/Algorithms.java
+++ Level-3/src/algorithm/Algorithms.java
@@ -5,7 +5,28 @@ import java.util.List;
 public class Algorithms {
 
 	public static int findBrokenEgg(List<String> eggs) {
-		// TODO Auto-generated method stub
+		int x = 0;
+		for (int i = eggs.size() - 1; i > -1; i--) {
+			if (eggs.get(i).equals("cracked")) {
+				x = i;
+			} else {
+
+			}
+		}
+		System.out.println("#1:" + x);
+		return 0;
+	}
+
+	public static int countPearls(List<Boolean> oysters) {
+		int x = 0;
+		for (int i = 0; i > oysters.size(); i++) {
+			if (oysters.get(i).equals("true")) {
+				x++;
+			} else {
+
+			}
+		}
+		System.out.println("#2:" + x);
 		return 0;
 	}
 
