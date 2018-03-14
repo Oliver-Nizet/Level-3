0bfff6eb8ca83398caddf8b4e0c9c311904de909
Oliver Nizet <League@AdministorsiMac.attlocal.net>
2018-03-13 18:25:12 -0700

    Class 2

--- Level-3/src/algorithm/Algorithms.java
+++ Level-3/src/algorithm/Algorithms.java
@@ -9,25 +9,48 @@ public class Algorithms {
 		for (int i = eggs.size() - 1; i > -1; i--) {
 			if (eggs.get(i).equals("cracked")) {
 				x = i;
-			} else {
-
 			}
 		}
-		System.out.println("#1:" + x);
-		return 0;
+		return x;
 	}
 
 	public static int countPearls(List<Boolean> oysters) {
 		int x = 0;
-		for (int i = 0; i > oysters.size(); i++) {
-			if (oysters.get(i).equals("true")) {
+		for (int i = 0; i < oysters.size(); i++) {
+			if (oysters.get(i) == true) {
 				x++;
-			} else {
-
 			}
 		}
-		System.out.println("#2:" + x);
-		return 0;
+		return x;
 	}
 
+	public static double findTallest(List<Double> peeps) {
+		double x = 0.0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if (peeps.get(i) > x) {
+				x = peeps.get(i);
+			}
+		}
+		return x;
+	}
+
+	public static Object findLongestWord(List<String> words) {
+		String x = "";
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).length() > x.length()) {
+				x = words.get(i);
+			}
+		}
+		return x;
+	}
+
+	public static Object containsSOS(List<String> message1) {
+		Boolean x = message1.contains(" ... --- ... ");
+		return x;
+	}
+
+	public static List<String> sortScores(List<Double> results) {
+
+		return null;
+	}
 }
